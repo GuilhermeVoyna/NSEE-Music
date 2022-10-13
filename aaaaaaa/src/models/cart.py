2a84dd35e0a3cf43e0d7345957e096212d07dde4
Guilherme <101269920+GuilhermeVoyna@users.noreply.github.com>
2022-10-13 08:38:49 -0300

    anotatations

--- aaaaaaa/src/models/cart.py
+++
@@ -1,8 +0,0 @@
-
-class Cart():
-    def __init__(self):
-        self._products=[]
-    def get_products(self):
-        return self._products
-    def __str__(self):
-        return self._products
